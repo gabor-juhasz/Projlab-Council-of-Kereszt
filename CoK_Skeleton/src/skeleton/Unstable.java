9e7baffc25b4966a2e7264a5235f7602218a6ba6
Gábor Juhász <jgabor1045@gmail.com>
2020-03-30 12:04:00 +0200

    Move use-case befejezese
    +setInWater(boolean value) kommentezese

--- CoK_Skeleton/src/skeleton/Unstable.java
+++ CoK_Skeleton/src/skeleton/Unstable.java
@@ -4,10 +4,21 @@ public class Unstable extends Ice {
 	
 	public void movedOn(Player p) {
 		System.out.println("Unstable.movedOn()");
+		
+		//Teszteseten kivul, normal korulmenyek kozott akkor fordul
+		//at, ha tullepjuk az instabil mezo teherbirasat (pl. mikor a
+		//4. babu ralep a 3 "kapacitasu" mezore)
+		this.flip();
 	}
 	
 	public void flip() {
 		System.out.println("Unstable.flip()");
 		
+		
+		Eskimo eskimo = new Eskimo();
+		
+		//True parameterrel a vizbe helyezzuk a babu(i)nkat,
+		//hiszen atfordul a jegtablank
+		eskimo.setInWater(true);
 	}
 }
